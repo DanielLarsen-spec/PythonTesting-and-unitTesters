bd6cbd2374f279dae7676d3dda232c0312f918b7
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-24 23:26:29 +0200

    Getting startet with Python and unittesters
    Doing some easy exercises with syntax and unitTesters

---
+++ pythonLearning/1_unitTester.py
@@ -0,0 +1,16 @@
+# Km to miles converter. 1 miles is 1.6 km.
+
+def kmToMilesConverter(km):
+    miles = km / 1.6
+    return miles
+
+
+def unitTester():
+    assert kmToMilesConverter(1) == 0.625, "Test case 1 failed"
+    assert kmToMilesConverter(2) == 1.25, "Test case 2 failed"
+    assert kmToMilesConverter(3) == 1.875, "Test case 3 failed"
+    assert kmToMilesConverter(4) == 2.5, "Test case 4 failed"
+    print("All test cases passed!")
+unitTester()    
+userInput = int(input("Enter the distance in km:"))
+print("The distance in miles is:", kmToMilesConverter(userInput))
---
+++ pythonLearning/exercises.py
@@ -0,0 +1,66 @@
+#Exercise 1: Variables 
+# x = 5
+# y = 10
+# sum = x + y
+# print("The sum of x og y us ", sum)
+
+#Exercise 2: Strings
+# name = "Daniel"
+# print("Hello", name)
+
+#Exercise 3 : Lists
+# fruits = ["orange", "Pear", "Banana"]
+# print("The secound fruit in my list is ", fruits[1])
+
+# #Exercise 4: Loops
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     print("The number is ", number)
+    
+# #Exercise 5: Functions
+
+# def add_numbers():
+#     x = 5
+#     y = 10
+#     sum = x + y
+#     print("The sum of x and y is ", sum)
+#     return sum
+# add_numbers()
+
+# #Exercise 6: conditionals
+
+# def check_number():
+#     number = 9
+#     if number > 10:
+#         print("The number is greater than 10")
+#     else:
+#         print("The number is less than or equal to 10")
+
+#     return number
+# check_number()
+
+# #Exercise 7: Dictionaries
+# person = {
+#     "name": "Larsen",
+#     "age": 28,
+#     "city": "Larvik"
+# }
+# print("Name:", person["name"],
+#       "Age:", person["age"],
+#       "City:", person["city"]
+#       )
+#Exercise 8: Classes
+# person = input("Your favorite color is: ",)
+# print("Your favorite color is: ", person)
+
+#Exercise 9: While Loops
+# count = 0
+# while count < 3:
+#     print("Count is:", count)
+#     count += 1
+
+#Exercise 10: Importing Modules
+import math
+
+result = math.sqrt(16)
+print("The square root of 16 is:", result)
